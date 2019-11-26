aeff23e2e9f0050e0a0efdbcdc93e4bae8ebec6f
Aaron-Doran <wesley.gossett@gmail.com>
2019-11-25 23:30:35 -0500

    Init commit. Added dummy data to TodoList.js.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+const data = [
+    {
+        task: 'Build a Todo App',
+        id: 1,
+        completed: false
+    },
+    {
+        task: 'Style Todo App',
+        id: 2,
+        completed: false
+    }
+];
